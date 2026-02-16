4427ffc38289cb6eab5ef9b74eb2116148087e03
Meryem Ebrahim <meryemebrahim.46@gmail.com>
2026-02-16 12:41:35 -0800

    resume analyzer prototype with styled UI and sample data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
 import type { Route } from "./+types/home";
 import NavBar from "../components/NavBar.jsx";
+import { resumes } from "../constants";
+import ResumeCard from "../components/ResumeCard";
+import { resume } from "react-dom/server";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumaid" },
@@ -8,13 +11,28 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('./public/images/bg-main.svg')] bg-cover">
+  return  <main className="bg-[url('./public/images/bg-main.svg')] bg-cover">
     <NavBar/>
     <section className="main-section " >
-      <div className="page-heading">
+      <div className="page-heading py-16">
       <h1>Track Your Application & Resume Ratings</h1>
 <h2>Review your sumbmissions and check AI-powered feedback</h2>
       </div>
+       {resumes.length >0 &&(
+      <div className="resumes-section ">
+
+  {
+    resumes.map((resume) => (
+      <div key={resume.id}>
+      <ResumeCard resume={resume} key={resume.id}/>
+      </div>
+    ))
+  }
+      </div>
+    )}
     </section>
+
+   
+
   </main>;
 }
